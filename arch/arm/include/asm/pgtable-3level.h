39468ae5c594f1e51c7e0c67ce18e1b375389004
Christoffer Dall <cdall@cs.columbia.edu>
2011-08-01 00:20:35 +0200

    ARM: KVM: Handle guest faults in KVM
    Handles the guest faults in KVM by mapping in corresponding user pages
    in the 2nd stage page tables.
    
    Introduces new ARM-specific kernel memory types, PAGE_KVM_GUEST and
    pgprot_guest variables used to map 2nd stage memory for KVM guests.
    
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

--- arch/arm/include/asm/pgtable-3level.h
+++ arch/arm/include/asm/pgtable-3level.h
@@ -156,4 +156,13 @@ static inline pmd_t *pmd_offset(pud_t *pud, unsigned long addr)
 
 #endif /* __ASSEMBLY__ */
 
+/*
+ * 2-nd stage PTE definitions for LPAE.
+ */
+#define L_PTE2_READ		(_AT(pteval_t, 1) << 6)	/* HAP[0] */
+#define L_PTE2_WRITE		(_AT(pteval_t, 1) << 7)	/* HAP[1] */
+#define L_PTE2_NORM_WB		(_AT(pteval_t, 3) << 4)	/* MemAttr[3:2] */
+#define L_PTE2_INNER_WB		(_AT(pteval_t, 3) << 2)	/* MemAttr[1:0] */
+
+
 #endif /* _ASM_PGTABLE_3LEVEL_H */
