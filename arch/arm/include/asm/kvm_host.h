3ee1326a0170c482bd577890a5b65f46bb4c2e1d
Marc Zyngier <marc.zyngier@arm.com>
2012-03-27 16:29:50 +0100

    ARM: KVM: Allow up to two virtual CPUs
    We do support SMP guests. And we want to hear about bugs if there is
    any...
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/include/asm/kvm_host.h
+++ arch/arm/include/asm/kvm_host.h
@@ -17,7 +17,7 @@
 #ifndef __ARM_KVM_HOST_H__
 #define __ARM_KVM_HOST_H__
 
-#define KVM_MAX_VCPUS 1
+#define KVM_MAX_VCPUS 2
 #define KVM_MEMORY_SLOTS 32
 #define KVM_PRIVATE_MEM_SLOTS 4
 #define KVM_COALESCED_MMIO_PAGE_OFFSET 1
