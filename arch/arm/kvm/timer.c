e35a31be68381fe422a26aa237799e96808ff1fd
Marc Zyngier <marc.zyngier@arm.com>
2012-04-28 20:08:50 +0100

    fixup! KVM timers, mostly working now
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/kvm/timer.c
+++ arch/arm/kvm/timer.c
@@ -25,7 +25,11 @@
 #include <asm/kvm_vgic.h>
 #include <asm/kvm_arch_timer.h>
 
-static int virt_timer_ppi = 27;	/* Hardcoded crap! */
+static const struct kvm_irq_level virt_timer_ppi = {
+	.irq	= 27,	/* Hardcoded crap! */
+	.level	= 1,
+};
+
 static struct timecounter *timecounter;
 static struct workqueue_struct *wqueue;
 
@@ -39,7 +43,7 @@ static void kvm_timer_inject_irq(struct kvm_vcpu *vcpu)
 	struct arch_timer_cpu *timer = &vcpu->arch.timer_cpu;
 
 	timer->cntv_ctl |= 1 << 1; /* Mask the interrupt in the guest */
-	kvm_vgic_inject_irq(vcpu->kvm, vcpu->vcpu_id, virt_timer_ppi);
+	kvm_vgic_inject_irq(vcpu->kvm, vcpu->vcpu_id, &virt_timer_ppi);
 }
 
 static irqreturn_t kvm_arch_timer_handler(int irq, void *dev_id)
