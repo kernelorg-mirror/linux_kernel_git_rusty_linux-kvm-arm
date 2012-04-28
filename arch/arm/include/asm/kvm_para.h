c2c6343d1a3590a670c4a61e15a6cce6109dd53f
Christoffer Dall <c.dall@virtualopensystems.com>
2012-04-28 19:59:36 +0100

    ARM: KVM: Initial skeleton to compile KVM support
    Targets KVM support for Cortex A-15 processors.
    
    Contains no real functionality but all the framework components,
    make files, header files and some tracing functionality.
    
    "Nothing to see here. Move along, move along..."
    
    Most functionality is in arch/arm/kvm/* or arch/arm/include/asm/kvm_*.h.
    
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

--- arch/arm/include/asm/kvm_para.h
+++ arch/arm/include/asm/kvm_para.h
@@ -1,1 +1,11 @@
+#ifndef _ASM_ARM_KVM_PARA_H
+#define _ASM_ARM_KVM_PARA_H
+
 #include <asm-generic/kvm_para.h>
+
+static inline unsigned int kvm_arch_para_features(void)
+{
+	return 0;
+}
+
+#endif /* _ASM_ARM_KVM_PARA_H */
