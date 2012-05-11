18c03d4fbf1da01e6744e404e168a98110c16101
Marc Zyngier <marc.zyngier@arm.com>
2012-05-11 12:12:28 +0100

    ARM: KVM: arch_timers: Wire the init code and config option
    It is now possible to select CONFIG_KVM_ARM_TIMER to enable the
    KVM architected timer support.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/kvm/arm.c
+++ arch/arm/kvm/arm.c
@@ -257,6 +257,7 @@ out:
 
 void kvm_arch_vcpu_free(struct kvm_vcpu *vcpu)
 {
+	kvm_timer_vcpu_terminate(vcpu);
 	kmem_cache_free(kvm_vcpu_cache, vcpu);
 }
 
@@ -293,6 +294,9 @@ int kvm_arch_vcpu_init(struct kvm_vcpu *vcpu)
 
 	/* Set up VGIC */
 	kvm_vgic_vcpu_init(vcpu);
+
+	/* Set up the timer */
+	kvm_timer_vcpu_init(vcpu);
 
 	return 0;
 }
@@ -833,6 +837,13 @@ static int init_hyp_mode(void)
 	if (!err)
 		vgic_present = true;
 
+	/*
+	 * Init HYP architected timer support
+	 */
+	err = kvm_timer_hyp_init();
+	if (err)
+		goto out_free_mappings;
+	
 	/*
 	 * Set the HVBAR to the virtual kernel address
 	 */
