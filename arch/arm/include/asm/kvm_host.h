42d739f47a4e00f8b1379addf995b7bd711bf0e1
Marc Zyngier <marc.zyngier@arm.com>
2012-05-11 11:43:07 +0100

    ARM: KVM: arch_timers: Add minimal infrastructure
    Add some very minimal architected timer related infrastructure.
    For the moment, we just provide empty structures, and enable/disable
    access to the physical timer across world switch.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm/include/asm/kvm_host.h
+++ arch/arm/include/asm/kvm_host.h
@@ -23,6 +23,7 @@
 #define KVM_COALESCED_MMIO_PAGE_OFFSET 1
 
 #include <asm/kvm_vgic.h>
+#include <asm/kvm_arch_timer.h>
 
 /* We don't currently support large pages. */
 #define KVM_HPAGE_GFN_SHIFT(x)	0
@@ -45,6 +46,9 @@ struct kvm_arch {
 
 	/* Interrupt controller */
 	struct vgic_dist	vgic;
+
+	/* Timer */
+	struct arch_timer_kvm	timer;
 };
 
 #define EXCEPTION_NONE      0
@@ -117,6 +121,7 @@ struct kvm_vcpu_arch {
 	u32 irq_lines;		/* IRQ and FIQ levels */
 	u32 wait_for_interrupts;
 	struct vgic_cpu vgic_cpu;
+	struct arch_timer_cpu timer_cpu;
 };
 
 struct kvm_vm_stat {
